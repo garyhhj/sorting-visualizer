7d8d2182ddd3cf588270f70db87dabf4b32b2b21
Gary Huang <gary_1327@yahoo.com>
2023-04-17 03:40:48 -0400

    quick sort algorithm

--- index.js
+++ index.js
@@ -36,37 +36,22 @@ function reset(){
         divs[i].remove(); 
     }
     
-    //number of bars to generate 
+    //generate divs 
     let nBars = Math.floor(screen.width / 14);  
     generateArr(nBars); 
-    console.log("nbars is ", nBars); 
-    console.log(arr); 
     for(let i = 0; i < nBars; ++i){
         //style the div 
         //let div = document.createElement("div"); 
         let bar = document.createElement("div"); 
         bar.classList.add("bar");
         bar.style.height = arr[i] + "px";  
-        barContainer.appendChild(bar); //(document.createElement("div")); 
-
-        //var container = document.getElementsByClassName("barContainer"); 
-
-        //container.createElement();
-        console.log("inserting stuff"); 
+        barContainer.appendChild(bar);
     }
-
-    //add the divs back in 
 }
 
 /****************
  * sorting algo
  ****************/
-//const delay = ms => new Promise(res => setTimeout(res, ms));
-
-// function delay(ms){
-//     return new Promise(res => setTimeout(res, ms)); 
-// }
-
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -79,7 +64,15 @@ async function swap(lhs, rhs){
     
     bars[lhs].style.backgroundColor = "lightGreen"; 
     bars[rhs].style.backgroundColor = "lightGreen"; 
+    
+    // try {
+    //     let response = sleep(10);
+    // }
+    // catch(e){
+    //     console.log(e); 
+    // }
     await sleep(10); 
+
     bars[lhs].style.backgroundColor = "#ddd2c9";
     bars[rhs].style.backgroundColor = "#ddd2c9";
     //swaps two elements with index lhs and rhs and perform animation   
@@ -88,47 +81,16 @@ async function swap(lhs, rhs){
     arr[rhs] = temp;   
 }
 
-// async function cmp(lhs, rhs){
-//     let bars = document.getElementsByClassName("bar"); 
 
-//     if(arr[lhs] < arr[rhs]){
 
-//         //adjust size 
-//         bars[lhs].style.height = arr[rhs].toString() + "px"; 
-//         bars[rhs].style.height = arr[lhs].toString() + "px"; 
-        
-//         bars[lhs].style.backgroundColor = "lightGreen"; 
-//         bars[rhs].style.backgroundColor = "lightGreen"; 
-//         await sleep(50); 
-//         bars[lhs].style.backgroundColor = "#ddd2c9";
-//         bars[rhs].style.backgroundColor = "#ddd2c9";
-
-//         //console.log("after 2000ms delay"); 
-//         await swap(lhs, rhs);
-    
-//     }
-//     else{
-//         bars[lhs].style.backgroundColor = "lightGreen"; 
-//         bars[rhs].style.backgroundColor = "lightGreen"; 
-//         await sleep(50); 
-//         bars[lhs].style.backgroundColor = "#ddd2c9";
-//         bars[rhs].style.backgroundColor = "#ddd2c9";
-//     }
-//     //console.log("end of cmp function"); 
-
-//     //await sleep(50); 
-//     //do some animation idk 
-
-//     //compare i and j and determine if need to swap or not and then animate 
+// function swap(lhs, rhs){
+//     let temp = arr[lhs]; 
+//     arr[lhs] = arr[rhs]; 
+//     arr[rhs] = temp;   
 // }
 
-async function sort(){
-    //setTimeout(function() {console.log("timeout for 5s");},  5000); 
-
-    //alert("sort"); 
-    console.log("staring arr"); 
-    console.log(arr); 
-   
+async function bubbleSort(){
+    
     let arrSize = arr.length; 
     for(let i = 0; i < arrSize; ++i){
         for(let j = 0; j < arrSize - i - 1; ++j){
@@ -137,11 +99,81 @@ async function sort(){
             }
         }
     }
+}
+
+// async function partition(lo, hi) {
     
+//     let pivot = arr[hi]; 
+//     let leftIndex = lo; 
+//     let rightIndex = hi - 1; 
+
+//     while(leftIndex <= rightIndex){
+//        if(arr[leftIndex] <= pivot){
+//             leftIndex += 1; 
+//        }
+//        else if(arr[rightIndex] >= pivot){
+//             rightIndex -= 1; 
+//        }
+//        else {
+//             await swap(leftIndex, rightIndex); 
+//             leftIndex += 1; 
+//             rightIndex += 1; 
+//        }
+//     }
+
+//     await swap(leftIndex, hi); 
+//     return leftIndex; 
+
+//     // await swap(hi, rightIndex); 
+//     // return rightIndex; 
+// }
+
+async function partition(lo, hi){
+    let pivot = arr[hi]; 
+    let leftPtr = lo; 
+    let rightPtr = hi - 1; 
+
+    while(leftPtr <= rightPtr){
+        if(arr[leftPtr] <= pivot){
+            ++leftPtr; 
+        }
+        else if(arr[rightPtr] >= pivot){
+            --rightPtr; 
+        }
+        else{
+            await swap(leftPtr, rightPtr); 
+            ++leftPtr; 
+            --rightPtr; 
+        }
+    }
+
+    await swap(leftPtr, hi); 
+    return leftPtr; 
+}
+
+async function quickSort(lo, hi){
+    if(lo < hi){
+        let pivotIndex = await partition(lo, hi); 
+        await quickSort(lo, pivotIndex - 1);
+        await quickSort(pivotIndex + 1, hi); 
+    }
+}
+
+async function sort(){
+    //setTimeout(function() {console.log("timeout for 5s");},  5000); 
+
+    //alert("sort"); 
+    console.log("staring arr"); 
+    console.log(arr); 
+
+    await bubbleSort();     
+    // await quickSort(0, arr.length - 1); 
 
     console.log("ending arr"); 
     console.log(arr); 
+
 }
+
 
 /****************
  * events
